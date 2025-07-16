15425bd932f59b6c23f3a06b997d033399276ea1
SAIESHGIT <saieshchuttani@gmail.com>
2025-07-16 14:56:39 +0530

    new

--- 7/write/index.js
+++ 7/write/index.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 let users = [
     {
         name : "saiesh",
-        age : 19,
+        age : 20,
         address : "Delhi"
     },
     {
@@ -11,7 +11,7 @@ let users = [
         address : "Mumbai"
     }
 ]
-fs.writeFile('users.txt',JSON.stringify(users),function(err) {
-    if (err) return console.error(err);
+fs.writeFile('../users.txt',JSON.stringify(users),function(err) {
+    if (err) return console.log(err);
     console.log('done');
 });
